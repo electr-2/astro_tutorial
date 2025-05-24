1fac1bb40899119398ac0b5eedbc5b5c281c29b2
electr-2 <i2512022@gl.cc.uec.ac.jp>
2025-05-24 15:17:55 +0900

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://electr2-helloworld.netlify.app",
+  integrations: [preact()],
+});
